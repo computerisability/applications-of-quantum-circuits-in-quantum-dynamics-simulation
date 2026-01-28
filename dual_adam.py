a77953041508e64e9706a7c531eff52a20c75565
computerisability <834640568@qq.com>
2026-01-28 21:17:29 +0800

    Add files via upload

---
+++ dual_adam.py
@@ -0,0 +1,131 @@
+import json
+import numpy as np
+import pickle
+from qiskit import Aer,transpile,QuantumCircuit ,qpy
+from qiskit.utils import QuantumInstance
+from qiskit.opflow import X, Z, PauliExpectation
+from hamiltonian import *
+from parametrized_circuit import * 
+from dualQRTE_adam import DualQRTE
+from qiskit.quantum_info import Statevector, partial_trace, entropy
+#from functools import partial
+
+n_qubits = 8
+k = int(n_qubits/2)
+
+tf = 2
+dt = 0.05
+times = np.linspace(0, tf, int(tf/dt) + 1)
+
+
+backend = Aer.get_backend("statevector_simulator") 
+instance = QuantumInstance(backend=backend, shots=1)
+
+psi0 = QuantumCircuit(n_qubits)
+fermion_idx = [0, 2, 4, 6 ]
+psi0.x(fermion_idx)  # Initial state |10101010⟩
+psi0.barrier()
+
+# Convert the initial state to a Statevector
+initial_state = Statevector.from_instruction(psi0)
+
+hamiltonian = hubbard_ladder_hamiltonian(size_x = 2, model_params=[1,0.8]) 
+hamiltonian = SparsePauliOp( hamiltonian.primitive.paulis, hamiltonian.primitive.coeffs )
+
+#ansatz = AnsatzSchwinger(n_qubits, depth=2)     
+ansatz = AnsatzLadderHubbard(n_qubits, depth = 1)   
+
+initial_parameters = np.zeros(ansatz.circuit.num_parameters)
+
+observables = [num_op(n_qubits, 0) @ num_op(n_qubits, 4),               #若8个比特，则将此处2，修改为4
+                num_op(n_qubits, 0) @ num_op(n_qubits, 4)]
+
+
+
+
+# Initialize the algorithm
+dual_qrte = DualQRTE(
+    ansatz,
+    maxiter=1000,
+    gradient_tolerance=1e-5,
+    expectation= PauliExpectation(),
+    quantum_instance=instance
+)
+
+# Evolve in time
+data=dual_qrte.evolve(
+        hamiltonian,
+        num_time_steps=int(np.ceil(tf/dt)),
+        final_time=tf,
+        initial_parameters=initial_parameters,
+        shift_init_guess=np.zeros(len(initial_parameters)),
+        observables=observables
+)
+
+
+print(data['evolved state'])
+print(f"the type of evolved state is:{type(data['evolved state'])}")
+
+evolved_states = data['evolved state']
+# 将 psi0 转换为状态向量
+tqc0 = transpile(psi0, backend)
+job0 = backend.run(tqc0)
+result0 = job0.result()
+psi0_statevector = result0.get_statevector()
+
+# 计算 每个 evolved_state 的纠缠熵
+entanglement_entropies = []
+for evolved_state in evolved_states:
+    # 计算
+    statevector = Statevector.from_instruction(evolved_state)
+    reduced_density_matrix = partial_trace(statevector, list(range(k)))
+    entanglement_entropy = entropy(reduced_density_matrix)
+    entanglement_entropies.append(entanglement_entropy)
+
+# 计算 psi0 与每个 evolved_state 的重叠度
+overlaps = []
+for evolved_state in evolved_states:
+    tqc = transpile(evolved_state, backend)
+    job = backend.run(tqc)
+    result = job.result()
+    evolved_statevector = result.get_statevector()
+
+    # 计算重叠度
+    sv1 = Statevector(psi0_statevector)
+    sv2 = Statevector(evolved_statevector)
+    overlap = abs(sv1.inner(sv2)) ** 2
+    overlaps.append(overlap)
+
+
+#fig4b +fig6 +fig7a
+observables_values = []
+for obs in data['observables values']:
+    observables_values.extend(obs.tolist())
+#n0n4 = observables_values[0::2]
+n0n4 = observables_values[1::2]                                    #若为8个比特，则这里还有
+
+
+
+loschmidt_echo = -1.0/n_qubits * np.log(overlaps)
+
+
+depth_local = data['Depth']
+cnot_local = data['CNOTS']
+number_local = data['parameter_number']
+
+
+np.savez('hubbard_dual_adam_.npz', n0n4 = n0n4,
+        entanglement_entropies = entanglement_entropies, loschmidt_echo = loschmidt_echo, 
+         depth_local=depth_local , cnot_local=cnot_local, number_local = number_local
+    )     
+
+a = data["evolved state"]
+with open('hubbard_dual_adam_.qpy', 'wb') as f:                              #量子电路算出来，需要保存为qpy文件
+    qpy.dump(a, f)  
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 21:10:14 2024
+
+@author: 83464
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 22:47:00 2024
+
+@author: 83464
+"""
+
+# Import the relevant libraries
+from qiskit.opflow import ListOp, StateFn
+import numpy as np
+from hamiltonian import I, Z, one_site_op
+from functools import reduce
+
+def adam_step(theta, t, m, v, grad):
+    """ One step of the Adam optimizer, following the structure and notation suggested in
+     https://arxiv.org/abs/1412.6980.
+
+    Args:
+        theta (np.array): parameters to be optimized
+        t (int): time step
+        m (np.array): 1st moment vector
+        v (np.array): 2nd moment vector
+        grad (np.array): gradient of the objective function to be optimized at time step t
+
+    Returns:
+        new_theta (np.array): updated parameters
+    """
+
+    # Default settings (except for alpha) suggested in the paper:
+    beta1 = 0.9
+    beta2 = 0.999
+    eps = 1e-8
+    alpha = 0.005
+
+    # Implement one step of the Adam optimizer
+    m = beta1*m + (1-beta1)*grad
+    v = beta2*v + (1-beta2)*grad**2
+    alphat = alpha * np.sqrt(1-beta2**t)/(1-beta1**t)
+    new_theta = theta - alphat * m/(np.sqrt(v) + eps)
+
+    return new_theta, m, v
+
+
+def get_observable_evaluator(
+        ansatz,
+        observables,
+        expectation,
+        sampler
+):
+    """ Compute the expectation value of a list of observables given a parametrized circuit.
+
+    Args:
+        ansatz (QuantumCircuit): parametrized/variational circuit.
+        observables (List[PauliOp]): list of Pauli strings to evaluate.
+        expectation (ExpectationBase): expectation converter to evaluate expectation values.
+        sampler (CircuitSampler): uses a backend to convert any StateFn into an approximation of the
+                                  state function.
+
+    Returns:
+        A function that computes the expectation value of a list of operators.
+    """
+    if isinstance(observables, list):
+        observables = ListOp(observables)
+
+    expectation_values = StateFn(observables, is_measurement=True) @ StateFn(ansatz)
+    expectation_values = expectation.convert(expectation_values)
+
+    ansatz_parameters = ansatz.parameters
+
+    def evaluate_observables(parameter_values: np.array):
+        """ Evaluate the observables given the numerical parameters provided. """
+        if len(parameter_values) != len(ansatz_parameters):
+            raise (f"The number of parameter values provided does not match the number of free "
+                   f"parameters in the circuit."
+                   )
+
+        value_dict = dict(zip(ansatz_parameters, parameter_values))
+        sampled_op = sampler.convert(expectation_values, params=value_dict)
+        return np.real(np.array(sampled_op.eval()))
+
+    return evaluate_observables
+
+
+def tens_prod(op_list):
+        """ Compute the tensor product of a list of PauliOp. """
+        return reduce(lambda x, y: x^y, op_list)
+
+
+def projector_zero_global(num_qubits):
+     return tens_prod([0.5 * (I + Z)] * num_qubits)
+
+
+def projector_zero_local(num_qubits):
+    tot_prj = 0  # initialize the sum of all local projectors
+
+    for k in range(num_qubits):
+        tot_prj += one_site_op(num_qubits, k, 0.5 * (I + Z))
+
+    return tot_prj / num_qubits  # returns a PauliSumOp object
+
+
+def time_convert(seconds):
+    min, sec = divmod(seconds, 60)
+    hour, min = divmod(min, 60)
+    return '%d:%02d:%02d' % (hour, min, sec)
